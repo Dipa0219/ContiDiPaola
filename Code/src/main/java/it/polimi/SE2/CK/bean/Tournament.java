e7aef6f3c3ef072db4de52d521eeb57d6bf5f0d9
Antonio Di Paola <antonio1.dipaola@mail.polimi.it>
2024-01-15 17:34:48 +0100

    PersonalHomePage - Code: Added tournament classes

---
+++ Code/src/main/java/it/polimi/SE2/CK/bean/Tournament.java
@@ -0,0 +1,60 @@
+package it.polimi.SE2.CK.bean;
+
+import java.sql.Timestamp;
+
+public class Tournament {
+    int id;
+    String name;
+    String description;
+    int creatorId;
+    String creatorUsername;
+    Timestamp regDeadline;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCreatorId() {
+        return creatorId;
+    }
+
+    public String getCreatorUsername() {
+        return creatorUsername;
+    }
+
+    public Timestamp getRegDeadline() {
+        return regDeadline;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCreatorId(int creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public void setCreatorUsername(String creatorUsername) {
+        this.creatorUsername = creatorUsername;
+    }
+
+    public void setRegDeadline(Timestamp regDeadline) {
+        this.regDeadline = regDeadline;
+    }
+}
