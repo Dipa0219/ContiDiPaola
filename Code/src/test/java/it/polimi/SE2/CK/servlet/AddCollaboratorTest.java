789667f4c277637a1d365ccfae453a7d80d5ef88
Antonio Di Paola <antonio1.dipaola@mail.polimi.it>
2024-01-27 11:57:58 +0100

    Tests - Code: added Join Tournament and first add Collaborator test

---
+++ Code/src/test/java/it/polimi/SE2/CK/servlet/AddCollaboratorTest.java
@@ -0,0 +1,163 @@
+package it.polimi.SE2.CK.servlet;
+
+
+import org.junit.Test;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Enumeration;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class AddCollaboratorTest {
+
+    @Test
+    public void test_getUser_doGet() throws ServletException, IOException {
+        // Mock HttpServletRequest and HttpServletResponse
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+
+        // Set the parameters for the request
+        StringWriter writer = new StringWriter();
+        when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
+        // Create an instance of LoginManager and invoke the doPost method
+        AddCollaborator addCollaborator = new AddCollaborator();
+
+        addCollaborator.doGet(request, response);
+
+        // Verify that the response status is 400
+        verify(response).setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+        // Verify that the error message is written to the response
+        assertEquals (writer.toString(),"Request non acceptable\r\n");
+    }
+
+    @Test
+    public void testDoPost() {
+    }
+
+    @Test
+    public void test_join_tournament_with_empty_session() throws IOException, ServletException {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        when(request.getSession()).thenReturn(new HttpSession() {
+            @Override
+            public long getCreationTime() {
+                return 0;
+            }
+
+            @Override
+            public String getId() {
+                return null;
+            }
+
+            @Override
+            public long getLastAccessedTime() {
+                return 0;
+            }
+
+            @Override
+            public ServletContext getServletContext() {
+                return null;
+            }
+
+            @Override
+            public void setMaxInactiveInterval(int i) {
+
+            }
+
+            @Override
+            public int getMaxInactiveInterval() {
+                return 0;
+            }
+
+            @Override
+            public HttpSessionContext getSessionContext() {
+                return null;
+            }
+
+            @Override
+            public Object getAttribute(String s) {
+                return null;
+            }
+
+            @Override
+            public Object getValue(String s) {
+                return null;
+            }
+
+            @Override
+            public Enumeration<String> getAttributeNames() {
+                return null;
+            }
+
+            @Override
+            public String[] getValueNames() {
+                return new String[0];
+            }
+
+            @Override
+            public void setAttribute(String s, Object o) {
+
+            }
+
+            @Override
+            public void putValue(String s, Object o) {
+
+            }
+
+            @Override
+            public void removeAttribute(String s) {
+
+            }
+
+            @Override
+            public void removeValue(String s) {
+
+            }
+
+            @Override
+            public void invalidate() {
+
+            }
+
+            @Override
+            public boolean isNew() {
+                return false;
+            }
+        });
+
+        StringWriter writer = new StringWriter();
+        when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
+        // Invoke doGet method
+        AddCollaborator addCollaborator = new AddCollaborator();
+
+        addCollaborator.doPost(request, response);
+        verify(response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        assertEquals(writer.toString(),"You can't access to this page\r\n");
+    }
+
+
+    private ServletConfig setUp() {
+        ServletContext servletContext = mock(ServletContext.class);
+
+        // Mock servlet config
+        ServletConfig servletConfig = mock(ServletConfig.class);
+        when(servletConfig.getServletContext()).thenReturn(servletContext);
+        when(servletContext.getInitParameter("dbUrl")).thenReturn("jdbc:mysql://localhost:3306/new_schema?serverTimezone=UTC");
+        when(servletContext.getInitParameter("dbUser")).thenReturn("root");
+        when(servletContext.getInitParameter("dbPassword")).thenReturn("");
+        when(servletContext.getInitParameter("dbDriver")).thenReturn("com.mysql.cj.jdbc.Driver");
+        return  servletConfig;
+    }
+}
--- Code/src/test/java/it/polimi/SE2/CK/servlet/JoinTournamentTest.java
+++ Code/src/test/java/it/polimi/SE2/CK/servlet/JoinTournamentTest.java
@@ -1,10 +1,6 @@
 package it.polimi.SE2.CK.servlet;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import it.polimi.SE2.CK.bean.Battle;
 import it.polimi.SE2.CK.bean.SessionUser;
-
 import org.junit.Test;
 
 import javax.servlet.ServletConfig;
@@ -14,25 +10,264 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionContext;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.sql.Timestamp;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.Enumeration;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
-public class ShowBattlesTest {
+public class JoinTournamentTest {
 
-
-    // Successfully retrieve battles by tournament ID
     @Test
-    public void test_retrieve_battles_by_tournament_id() throws IOException, ServletException, ParseException {
+    public void test_getUser_doGet() throws ServletException, IOException {
+        // Mock HttpServletRequest and HttpServletResponse
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+
+        // Set the parameters for the request
+        StringWriter writer = new StringWriter();
+        when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
+        // Create an instance of LoginManager and invoke the doPost method
+        JoinTournament joinTournament = new JoinTournament();
+
+        joinTournament.doGet(request, response);
+
+        // Verify that the response status is 400
+        verify(response).setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+        // Verify that the error message is written to the response
+        assertEquals (writer.toString(),"Request non acceptable\r\n");
+    }
+
+    @Test
+    public void test_join_tournament() throws IOException, ServletException{
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        when(request.getSession()).thenReturn(new HttpSession() {
+            @Override
+            public long getCreationTime() {
+                return 0;
+            }
+
+            @Override
+            public String getId() {
+                return null;
+            }
+
+            @Override
+            public long getLastAccessedTime() {
+                return 0;
+            }
+
+            @Override
+            public ServletContext getServletContext() {
+                return null;
+            }
+
+            @Override
+            public void setMaxInactiveInterval(int i) {
+
+            }
+
+            @Override
+            public int getMaxInactiveInterval() {
+                return 0;
+            }
+
+            @Override
+            public HttpSessionContext getSessionContext() {
+                return null;
+            }
+
+            @Override
+            public Object getAttribute(String s) {
+                SessionUser user= new SessionUser();
+                user.setId(4);
+                user.setUsername("ValeTuttoPane");
+                user.setRole(1);
+                return user;
+            }
+
+            @Override
+            public Object getValue(String s) {
+                return null;
+            }
+
+            @Override
+            public Enumeration<String> getAttributeNames() {
+                return null;
+            }
+
+            @Override
+            public String[] getValueNames() {
+                return new String[0];
+            }
+
+            @Override
+            public void setAttribute(String s, Object o) {
+
+            }
+
+            @Override
+            public void putValue(String s, Object o) {
+
+            }
+
+            @Override
+            public void removeAttribute(String s) {
+
+            }
+
+            @Override
+            public void removeValue(String s) {
+
+            }
+
+            @Override
+            public void invalidate() {
+
+            }
+
+            @Override
+            public boolean isNew() {
+                return false;
+            }
+        });
+
+        // Set up the request parameters
+        when(request.getParameter("TournamentId")).thenReturn("1");
+
+
+        // Create an instance of ShowBattles and invoke the doGet method
+        JoinTournament joinTournament = new JoinTournament();
+        ServletConfig servletConfig =setUp();
+        joinTournament.init(servletConfig);
+
+        joinTournament.doPost(request, response);
+
+        // Verify that the response status, content type, and character encoding are set correctly
+        verify(response).setStatus(HttpServletResponse.SC_OK);
+        verify(response).setContentType("application/json");
+        verify(response).setCharacterEncoding("UTF-8");
+    }
+
+    @Test
+    public void test_join_tournament_made_by_educator() throws IOException, ServletException{
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        when(request.getSession()).thenReturn(new HttpSession() {
+            @Override
+            public long getCreationTime() {
+                return 0;
+            }
+
+            @Override
+            public String getId() {
+                return null;
+            }
+
+            @Override
+            public long getLastAccessedTime() {
+                return 0;
+            }
+
+            @Override
+            public ServletContext getServletContext() {
+                return null;
+            }
+
+            @Override
+            public void setMaxInactiveInterval(int i) {
+
+            }
+
+            @Override
+            public int getMaxInactiveInterval() {
+                return 0;
+            }
+
+            @Override
+            public HttpSessionContext getSessionContext() {
+                return null;
+            }
+
+            @Override
+            public Object getAttribute(String s) {
+                SessionUser user= new SessionUser();
+                user.setId(2);
+                user.setUsername("MarielloBello");
+                user.setRole(0);
+                return user;
+            }
+
+            @Override
+            public Object getValue(String s) {
+                return null;
+            }
+
+            @Override
+            public Enumeration<String> getAttributeNames() {
+                return null;
+            }
+
+            @Override
+            public String[] getValueNames() {
+                return new String[0];
+            }
+
+            @Override
+            public void setAttribute(String s, Object o) {
+
+            }
+
+            @Override
+            public void putValue(String s, Object o) {
+
+            }
+
+            @Override
+            public void removeAttribute(String s) {
+
+            }
+
+            @Override
+            public void removeValue(String s) {
+
+            }
+
+            @Override
+            public void invalidate() {
+
+            }
+
+            @Override
+            public boolean isNew() {
+                return false;
+            }
+        });
+        StringWriter writer = new StringWriter();
+        when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
+        // Set up the request parameters
+        when(request.getParameter("TournamentId")).thenReturn("1");
+
+
+        // Create an instance of ShowBattles and invoke the doGet method
+        JoinTournament joinTournament = new JoinTournament();
+        ServletConfig servletConfig =setUp();
+        joinTournament.init(servletConfig);
+
+        joinTournament.doPost(request, response);
+
+        verify(response).setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        assertEquals(writer.toString(),"Educator are not allowed to join tournament in this way, please try again\r\n");
+    }
+
+    @Test
+    public void test_join_tournament_already_subscripted() throws IOException, ServletException{
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
         when(request.getSession()).thenReturn(new HttpSession() {
@@ -133,150 +368,19 @@ public class ShowBattlesTest {
 
 
         // Create an instance of ShowBattles and invoke the doGet method
-        ShowBattles showBattles = new ShowBattles();
+        JoinTournament joinTournament = new JoinTournament();
         ServletConfig servletConfig =setUp();
-        showBattles.init(servletConfig);
+        joinTournament.init(servletConfig);
 
-        showBattles.doGet(request, response);
+        joinTournament.doPost(request, response);
 
-        ArrayList <Battle> battles= new ArrayList<>();
-        Battle battle= new Battle();
-        battle.setId(1);
-        battle.setName("Introduction");
-        battle.setDescription("First exercises");
-        SimpleDateFormat dateTimeFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        battle.setRegDeadline(new Timestamp((dateTimeFormatter.parse("2024-03-12 10:00:00").getTime())));
-        battle.setSubDeadline(new Timestamp(dateTimeFormatter.parse("2024-04-01 11:00:00").getTime()));
-        battle.setMinNumStudent(1);
-        battle.setMaxNumStudent(3);
-        battles.add(battle);
-    
         // Verify that the response status, content type, and character encoding are set correctly
-        verify(response).setStatus(HttpServletResponse.SC_OK);
-        verify(response).setContentType("application/json");
-        verify(response).setCharacterEncoding("UTF-8");
-    
-        // Verify that the battles are converted to JSON and written to the response
-        Gson gson = new GsonBuilder().create();
-        String json = gson.toJson(battles);
-        assertEquals(json, writer.toString());
+        verify(response).setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        assertEquals(writer.toString(),"You have already joined this tournament\r\n");
     }
 
     @Test
-    public void test_tournament_id_is_not_number() throws IOException, ServletException {
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        when(request.getSession()).thenReturn(new HttpSession() {
-            @Override
-            public long getCreationTime() {
-                return 0;
-            }
-
-            @Override
-            public String getId() {
-                return null;
-            }
-
-            @Override
-            public long getLastAccessedTime() {
-                return 0;
-            }
-
-            @Override
-            public ServletContext getServletContext() {
-                return null;
-            }
-
-            @Override
-            public void setMaxInactiveInterval(int i) {
-
-            }
-
-            @Override
-            public int getMaxInactiveInterval() {
-                return 0;
-            }
-
-            @Override
-            public HttpSessionContext getSessionContext() {
-                return null;
-            }
-
-            @Override
-            public Object getAttribute(String s) {
-                SessionUser user= new SessionUser();
-                user.setId(1);
-                user.setUsername("Bob99");
-                user.setRole(1);
-                return user;
-            }
-
-            @Override
-            public Object getValue(String s) {
-                return null;
-            }
-
-            @Override
-            public Enumeration<String> getAttributeNames() {
-                return null;
-            }
-
-            @Override
-            public String[] getValueNames() {
-                return new String[0];
-            }
-
-            @Override
-            public void setAttribute(String s, Object o) {
-
-            }
-
-            @Override
-            public void putValue(String s, Object o) {
-
-            }
-
-            @Override
-            public void removeAttribute(String s) {
-
-            }
-
-            @Override
-            public void removeValue(String s) {
-
-            }
-
-            @Override
-            public void invalidate() {
-
-            }
-
-            @Override
-            public boolean isNew() {
-                return false;
-            }
-        });
-        StringWriter writer = new StringWriter();
-        when(response.getWriter()).thenReturn(new PrintWriter(writer));
-
-        // Set up the request parameters
-        when(request.getParameter("TournamentId")).thenReturn("a");
-
-
-        // Create an instance of ShowBattles and invoke the doGet method
-        ShowBattles showBattles = new ShowBattles();
-        ServletConfig servletConfig =setUp();
-        showBattles.init(servletConfig);
-
-        showBattles.doGet(request, response);
-
-        // Verify that the response status, content type, and character encoding are set correctly
-        verify(response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        assertEquals(writer.toString(),"Internal error with the page, please try again\r\n");
-    }
-
-    @Test
-    public void test_tournament_id_is_0_or_less() throws IOException, ServletException {
+    public void test_join_tournament_negative_id() throws IOException, ServletException{
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
         when(request.getSession()).thenReturn(new HttpSession() {
@@ -377,19 +481,132 @@ public class ShowBattlesTest {
 
 
         // Create an instance of ShowBattles and invoke the doGet method
-        ShowBattles showBattles = new ShowBattles();
+        JoinTournament joinTournament = new JoinTournament();
         ServletConfig servletConfig =setUp();
-        showBattles.init(servletConfig);
+        joinTournament.init(servletConfig);
 
-        showBattles.doGet(request, response);
+        joinTournament.doPost(request, response);
 
         // Verify that the response status, content type, and character encoding are set correctly
-        verify(response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        verify(response).setStatus(HttpServletResponse.SC_BAD_REQUEST);
         assertEquals(writer.toString(),"Internal error with the page, please try again\r\n");
     }
 
     @Test
-    public void test_checkUsername_with_empty_session() throws IOException, ServletException {
+    public void test_join_tournament_wrong_format() throws IOException, ServletException {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        when(request.getSession()).thenReturn(new HttpSession() {
+            @Override
+            public long getCreationTime() {
+                return 0;
+            }
+
+            @Override
+            public String getId() {
+                return null;
+            }
+
+            @Override
+            public long getLastAccessedTime() {
+                return 0;
+            }
+
+            @Override
+            public ServletContext getServletContext() {
+                return null;
+            }
+
+            @Override
+            public void setMaxInactiveInterval(int i) {
+
+            }
+
+            @Override
+            public int getMaxInactiveInterval() {
+                return 0;
+            }
+
+            @Override
+            public HttpSessionContext getSessionContext() {
+                return null;
+            }
+
+            @Override
+            public Object getAttribute(String s) {
+                SessionUser user= new SessionUser();
+                user.setId(1);
+                user.setUsername("Bob99");
+                user.setRole(1);
+                return user;
+            }
+
+            @Override
+            public Object getValue(String s) {
+                return null;
+            }
+
+            @Override
+            public Enumeration<String> getAttributeNames() {
+                return null;
+            }
+
+            @Override
+            public String[] getValueNames() {
+                return new String[0];
+            }
+
+            @Override
+            public void setAttribute(String s, Object o) {
+
+            }
+
+            @Override
+            public void putValue(String s, Object o) {
+
+            }
+
+            @Override
+            public void removeAttribute(String s) {
+
+            }
+
+            @Override
+            public void removeValue(String s) {
+
+            }
+
+            @Override
+            public void invalidate() {
+
+            }
+
+            @Override
+            public boolean isNew() {
+                return false;
+            }
+        });
+        StringWriter writer = new StringWriter();
+        when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
+        // Set up the request parameters
+        when(request.getParameter("TournamentId")).thenReturn("a");
+
+
+        // Create an instance of ShowBattles and invoke the doGet method
+        JoinTournament joinTournament = new JoinTournament();
+        ServletConfig servletConfig =setUp();
+        joinTournament.init(servletConfig);
+
+        joinTournament.doPost(request, response);
+
+        // Verify that the response status, content type, and character encoding are set correctly
+        verify(response).setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        assertEquals(writer.toString(),"Internal error with the page, please try again\r\n");
+    }
+
+    @Test
+    public void test_join_tournament_with_empty_session() throws IOException, ServletException {
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
         when(request.getSession()).thenReturn(new HttpSession() {
@@ -483,32 +700,11 @@ public class ShowBattlesTest {
         when(response.getWriter()).thenReturn(new PrintWriter(writer));
 
         // Invoke doGet method
-        ShowBattles showBattles = new ShowBattles();
-        showBattles.doGet(request, response);
+        JoinTournament joinTournament = new JoinTournament();
+        joinTournament.doPost(request,response);
 
         verify(response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         assertEquals(writer.toString(),"You can't access to this page\r\n");
-    }
-
-    @Test
-    public void test_getUser_doPost() throws ServletException, IOException {
-        // Mock HttpServletRequest and HttpServletResponse
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-
-        // Set the parameters for the request
-        StringWriter writer = new StringWriter();
-        when(response.getWriter()).thenReturn(new PrintWriter(writer));
-
-        // Create an instance of LoginManager and invoke the doPost method
-        ShowBattles showBattles = new ShowBattles();
-
-        showBattles.doPost(request, response);
-
-        // Verify that the response status is 400
-        verify(response).setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
-        // Verify that the error message is written to the response
-        assertEquals (writer.toString(),"Request non acceptable\r\n");
     }
 
     private ServletConfig setUp() {
